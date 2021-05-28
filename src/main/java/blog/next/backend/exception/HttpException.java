62db2642da1743a6d95cb88a7de3f417d70333e4
qixi <qixi@beihai365.com>
2021-05-28 16:27:29 +0800

    update

--- src/main/java/blog/next/backend/exception/HttpException.java
+++ src/main/java/blog/next/backend/exception/HttpException.java
@@ -3,6 +3,7 @@ package blog.next.backend.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+//抛出的异常，底层会根据状态码，重定向到4xx、5xx页面，如果是接口那就是json数据
 @ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "自定义一个403错误信息")
 public class HttpException extends RuntimeException {
 
